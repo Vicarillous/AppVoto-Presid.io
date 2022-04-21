3f0c87914cf800b27ca13ee03f2b2194b4a27af9
Vinícius Marques <marquesv473@gmail.com>
2022-04-20 21:41:12 -0400

    Atualizado android studio

--- app/src/main/java/com/example/appvoto/MainActivity.kt
+++ app/src/main/java/com/example/appvoto/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +25,8 @@ class MainActivity : AppCompatActivity() {
     fun calcDate() {
         val dataNasc = SimpleDateFormat("dd/MM/yyyy")
         val dataAtual = SimpleDateFormat("dd/MM/yyyy")
-        val dataNascimento = dataNasc.parse("01/01/2000")
-        val dataAtualizada = dataAtual.parse("01/01/2020")
+        val dataNascimento = dataNasc.parse("25/06/2005")
+        val dataAtualizada = Calendar.getInstance().time
         val diferenca = dataAtualizada.time - dataNascimento.time
         val dias = diferenca / (1000 * 60 * 60 * 24)
         val idade = dias / 365
--- app/src/main/java/com/example/appvoto/Pessoa.kt
+++ app/src/main/java/com/example/appvoto/Pessoa.kt
@@ -1,13 +1,14 @@
 package com.example.appvoto
 
 import java.text.SimpleDateFormat
+import java.util.*
 
 class Pessoa {
     fun calcDate() {
         val dataNasc = SimpleDateFormat("dd/MM/yyyy")
         val dataAtual = SimpleDateFormat("dd/MM/yyyy")
         val dataNascimento = dataNasc.parse("01/01/2000")
-        val dataAtualizada = dataAtual.parse("01/01/2020")
+        val dataAtualizada = Calendar.getInstance().time
         val diferenca = dataAtualizada.time - dataNascimento.time
         val dias = diferenca / (1000 * 60 * 60 * 24)
         val idade = dias / 365
